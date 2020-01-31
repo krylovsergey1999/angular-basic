8b67c14ac73624c8ef9fbb4e511bc27037a14236
krylovss <krylovss@aurrum.pro>
2020-01-31 19:47:33 +0300

    ngif else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello world';
-  img = 'https://ru.reactjs.org/logo-og.png';
-
-  constructor() {
-    setTimeout(() => {
-      console.log('Timeout is over');
-      // tslint:disable-next-line:max-line-length
-      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/768px-Angular_full_color_logo.svg.png';
-    }, 5000);
-  }
+  backgroundToggle: boolean = false;
 }
