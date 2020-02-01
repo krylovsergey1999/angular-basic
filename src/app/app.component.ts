a8c0bebc712643b4b55d50ecc9b22014444c1e78
Krylov Sergey <47079612+krylovsergey1999@users.noreply.github.com>
2020-02-01 13:11:20 +0300

    Компоненты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string,
+  id?: any
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  backgroundToggle: boolean = false;
+  posts: Post[] = [
+    {
+      title: 'one',
+      text: '1'
+    },
+    {
+      title: 'two',
+      text: '2'
+    },
+    {
+      title: 'three',
+      text: '3'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {PostComponent} from './post/post.component';
-import { Post2Component } from './post2/post2.component';
-import { Post3Component } from './post3/post3.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    Post2Component,
-    Post3Component
+    PostFormComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
