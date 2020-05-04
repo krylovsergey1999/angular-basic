d8b88741cfd323b94f016732c8657b67cc0c15a9
Krylov Sergey <kssergeyru@gmail.com>
2020-05-04 08:08:21 +0300

    Работа с группой

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +16,8 @@ export class AppComponent implements OnInit {
       address: new FormGroup({
         country: new FormControl('ru'),
         city: new FormControl('', [Validators.required])
-      })
+      }),
+      skills: new FormArray([])
     });
   }
 
@@ -42,6 +43,12 @@ export class AppComponent implements OnInit {
           city: city
         }
     });
+  }
+
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+    //(<FormArray>this.form.get('skills'))
+    (this.form.get('skills') as FormArray).push(control);
   }
 }
 
