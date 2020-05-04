842fec94b6d0f08524a4486ff891068cd1e29149
Krylov Sergey <kssergeyru@gmail.com>
2020-05-04 21:17:05 +0300

    Работа с параметрами

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Post, PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  private post: Post;
 
+  constructor(private route: ActivatedRoute,
+              private postService: PostsService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params);
+      this.post = this.postService.getById(+params.id);
+    });
+  }
+
+  loadPost() {
+    this.router.navigate(['/post', 44]);
+  }
 }
